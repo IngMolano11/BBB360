3b819328bbc2d5f7756f10db8b29b97a1230a42d
Camilo Molano <ing.camilo.molano@gmail.com>
2025-03-31 19:50:18 -0500

    Componentes principales

---
+++ src/app/components/result-box/result-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-result-box',
+  imports: [],
+  templateUrl: './result-box.component.html',
+  styleUrl: './result-box.component.css'
+})
+export class ResultBoxComponent {
+
+}
