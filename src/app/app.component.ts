4aa1da396d1a22d756a8fb0ac9369baef4f335d3
Camilo Molano <ing.camilo.molano@gmail.com>
2025-04-02 15:29:40 -0500

    Componente de la barra de busqueda completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { ResultBoxComponent } from './components/result-box/result-box.component';
+import { FilterComponent } from './components/filter/filter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    SearchBoxComponent,
+    ResultBoxComponent,
+    FilterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'BBB360';
+  title = 'Compara a tu estilo';
 }
