0001514b8c37c2bb4a911c669d3c3b0f970d89b0
Camilo Molano <ing.camilo.molano@gmail.com>
2025-04-02 18:01:42 -0500

    Se agrega componente para filtros Parcia (pendiente nuevas funcionalidades)

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
+  showRanges = false;
 
+  toggleFilter(): void {
+    this.showRanges = !this.showRanges;
+  }
 }
